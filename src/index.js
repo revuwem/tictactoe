1670063c3f1aaf8ed5b4d6b63829a72484b520e7
revuwem <revuwem@gmail.com>
2020-08-21 15:13:48 +0300

    lifted square state up, made squares functional, added turns state

--- src/index.js
+++ src/index.js
@@ -3,35 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            value: null
-        }
-    }
-
-    render(){
-        return(
-            <button 
-                className="square" 
-                onClick={()=>this.setState({value: 'X'})}
-            >
-                {this.state.value}
-            </button>
-        );
-    }
+const Square = ({value, onClick}) => {
+    return (
+        <button
+            className="square"
+            onClick={onClick}
+        >
+            {value}
+        </button>
+    );
 }
 
-class Board extends React.Component{
-    renderSquare(i){
-        return <Square value={i}/>;
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+
     }
 
-    render(){
-        const status='Next player is: X';
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({ 
+            squares: squares,
+            xIsNext: !this.state.xIsNext 
+        });
+    }
 
-        return(
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />);
+    }
+
+    render() {
+        const status = 'Next player is: '+(this.state.xIsNext ? 'X' : 'O');
+
+        return (
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
@@ -54,9 +68,9 @@ class Board extends React.Component{
     }
 }
 
-class Game extends React.Component{
-    render(){
-        return(
+class Game extends React.Component {
+    render() {
+        return (
             <div className="game">
                 <div className="game-board">
                     <Board />
